ae900c277765d0fdad2b9f2ab588facf7ec32f1a
Adam <adamsurya23@if,unjani.ac.id>
2025-12-13 13:34:15 +0700

    Upload

--- pages/anggota/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../../config/database.php";
-
-$id = $_GET['id'];
-
-mysqli_query($conn, "DELETE FROM anggota_perpus WHERE id='$id'");
-header("Location: index.php");
-exit;
--- pages/daftar_buku/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../../config/database.php";
-
-$kode = $_GET['kode'];
-mysqli_query($conn, "DELETE FROM daftar_buku WHERE kode_buku='$kode'");
-
-header("Location: index.php");
-exit;
